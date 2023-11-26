ca94d902d2b17ad1cedf01dc15304d7169e1fcf4
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-11-26 13:44:03 +0200

    Update tour

--- src/shared/tour.ts
+++ src/shared/tour.ts
@@ -1,24 +1,11 @@
 export const steps = [
   {
-    selector: "#link-container",
+    selector: "#shareableData",
     content: "Add a link, text or image that you'd like to share",
   },
   {
-    selector: "#text-container",
-    content: "Add a link, text or image that you'd like to share",
-  },
-  {
-    selector: "#url-container",
-    content: "Add a link, text or image that you'd like to share",
-  },
-  {
-    selector: "#twitter",
+    selector: "#socialLinks",
     content:
       "Copy and use shareable link for twitter and other social media .... That's it, easy.",
-  },
-  {
-    selector: "#linkedin",
-    content:
-      "Copy and use shareable link for linkedin and other social media .... That's it, easy.",
   },
 ];
